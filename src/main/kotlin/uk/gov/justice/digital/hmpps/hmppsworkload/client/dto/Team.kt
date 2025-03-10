e40771ffe39eef31d2155ce168bec3f45cbc6107
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-03-10 08:10:55 +0000

    linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/client/dto/Team.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/client/dto/Team.kt
@@ -6,22 +6,22 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class Team @JsonCreator constructor(
   @JsonProperty("code") val code: String,
   @JsonProperty("description") val description: String,
-  @JsonProperty("localAdminUnit") val localAdminUnit: LocalAdminUnit
+  @JsonProperty("localAdminUnit") val localAdminUnit: LocalAdminUnit,
 )
 
 data class LocalAdminUnit @JsonCreator constructor(
   @JsonProperty("code") val code: String,
   @JsonProperty("description") val description: String,
-  @JsonProperty("probationDeliveryUnit") val probationDeliveryUnit: ProbationDeliveryUnit
+  @JsonProperty("probationDeliveryUnit") val probationDeliveryUnit: ProbationDeliveryUnit,
 )
 
 data class ProbationDeliveryUnit @JsonCreator constructor(
   @JsonProperty("code") val code: String,
   @JsonProperty("description") val description: String,
-  @JsonProperty("provider") val provider: Provider
+  @JsonProperty("provider") val provider: Provider,
 )
 
 data class Provider @JsonCreator constructor(
   @JsonProperty("code") val code: String,
-  @JsonProperty("description") val description: String
+  @JsonProperty("description") val description: String,
 )
