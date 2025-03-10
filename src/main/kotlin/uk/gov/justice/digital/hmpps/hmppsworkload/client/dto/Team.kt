bdb3bd3ef3d891e3fd63654a8ae647ac9f05b6d6
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2025-03-10 08:02:49 +0000

    Wfp 2975 additional logging (#635)
    * security fixes bumping spring boot version
    
    * wfp-2975 additional logging for PE data relating to allocation
    
    * linting
    
    * fixing tests
    
    * changing object
    
    * changing web client method
    
    * adding constructors

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/client/dto/Team.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/client/dto/Team.kt
@@ -1,24 +1,27 @@
 package uk.gov.justice.digital.hmpps.hmppsworkload.client.dto
 
-data class Team(
-  val code: String,
-  val description: String,
-  val localAdminUnit: LocalAdminUnit,
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class Team @JsonCreator constructor(
+  @JsonProperty("code") val code: String,
+  @JsonProperty("description") val description: String,
+  @JsonProperty("localAdminUnit") val localAdminUnit: LocalAdminUnit
 )
 
-data class LocalAdminUnit(
-  val code: String,
-  val description: String,
-  val probationDeliveryUnit: ProbationDeliveryUnit,
+data class LocalAdminUnit @JsonCreator constructor(
+  @JsonProperty("code") val code: String,
+  @JsonProperty("description") val description: String,
+  @JsonProperty("probationDeliveryUnit") val probationDeliveryUnit: ProbationDeliveryUnit
 )
 
-data class ProbationDeliveryUnit(
-  val code: String,
-  val description: String,
-  val provider: Provider,
+data class ProbationDeliveryUnit @JsonCreator constructor(
+  @JsonProperty("code") val code: String,
+  @JsonProperty("description") val description: String,
+  @JsonProperty("provider") val provider: Provider
 )
 
-data class Provider(
-  val code: String,
-  val description: String,
-)
+data class Provider @JsonCreator constructor(
+  @JsonProperty("code") val code: String,
+  @JsonProperty("description") val description: String
+  
