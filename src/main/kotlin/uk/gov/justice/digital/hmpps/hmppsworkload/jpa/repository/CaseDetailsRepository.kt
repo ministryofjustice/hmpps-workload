55c52ad38d228fe284afdf29c291546c99460fd1
Gary Reid <159784838+GaReid@users.noreply.github.com>
2025-08-06 09:50:31 +0100

    WFP-2825 improve duplicate message handling (#678)
    * WFP-2825 Failing tests
    
    * tests failing
    
    * Fix some tests
    
    * WFP-2825 Duplicate message handling
    
    * WFP-2825 Duplicate message handling

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/repository/CaseDetailsRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/jpa/repository/CaseDetailsRepository.kt
@@ -1,6 +1,25 @@
 package uk.gov.justice.digital.hmpps.hmppsworkload.jpa.repository
 
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.entity.CaseDetailsEntity
 
-interface CaseDetailsRepository : CrudRepository<CaseDetailsEntity, String>
+interface CaseDetailsRepository : CrudRepository<CaseDetailsEntity, String> {
+
+  @Suppress("LongParameterList")
+  @Modifying
+  @Query(
+    value = """INSERT INTO case_details(first_name, surname, tier, "type", crn) VALUES (:firstName, :surname, :tier, :caseType, :crn)
+     ON CONFLICT (crn) DO UPDATE  set first_name = excluded.first_name, surname = excluded.surname, tier = excluded.tier, type = excluded.type, crn = excluded.crn;""",
+    nativeQuery = true,
+  )
+  fun insertCaseDetails(
+    @Param("firstName") firstName: String,
+    @Param("surname") surname: String,
+    @Param("tier") tier: String,
+    @Param("caseType") caseType: String,
+    @Param("crn") crn: String,
+  )
+}
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/integration/IntegrationTestBase.kt
@@ -63,6 +63,7 @@ import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.repository.WorkloadCalcula
 import uk.gov.justice.digital.hmpps.hmppsworkload.jpa.repository.WorkloadPointsRepository
 import uk.gov.justice.digital.hmpps.hmppsworkload.listener.HmppsOffenderEvent
 import uk.gov.justice.digital.hmpps.hmppsworkload.service.AuditMessage
+import uk.gov.justice.digital.hmpps.hmppsworkload.service.SaveCasesDbService
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
 import uk.gov.justice.hmpps.sqs.MissingQueueException
 import java.math.BigDecimal
@@ -96,6 +97,9 @@ abstract class IntegrationTestBase {
 
   @Autowired
   protected lateinit var caseDetailsRepository: CaseDetailsRepository
+
+  @Autowired
+  protected lateinit var casesDbService: SaveCasesDbService
 
   @Autowired
   protected lateinit var requirementManagerRepository: RequirementManagerRepository
