db2b1e662ffd91dae99a64507a784b45eb3d1c80
Gary Reid <gary.reid@digital.justice.gov.uk>
2025-09-30 15:41:59 +0100

    Add test

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsworkload/integration/AuditQueueTest.kt
@@ -0,0 +1,20 @@
+package uk.gov.justice.digital.hmpps.hmppsworkload.integration
+
+import org.junit.jupiter.api.Test
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.BodyInserters
+import uk.gov.justice.digital.hmpps.hmppsworkload.service.ReductionsAuditData
+
+class AuditQueueTest : IntegrationTestBase() {
+  @Test
+  fun `should post audit`() {
+    val auditData = ReductionsAuditData("fred", 1)
+    webTestClient.post()
+      .uri("/audit/data")
+      .headers { it.authToken(roles = listOf("ROLE_QUEUE_WORKLOAD_ADMIN")) }
+      .body(BodyInserters.fromValue(auditData))
+      .accept(MediaType.APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isOk
+  }
+}
